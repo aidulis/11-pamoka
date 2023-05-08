9e60b684c2e421cda9dd3619dfd33928ed829aa9
aidulis <aidas.narkevicius@gmail.com>
2023-05-08 18:26:16 +0300

    11 pamokos failai

---
+++ antra-dalis.js
@@ -0,0 +1,9 @@
+window.onload = function () {
+    let x = -47;
+    console.log(x);
+    let y = 25 + x;
+    console.log(y);
+    let c = x + y;
+    console.log(c);
+    console.log(x + y);
+}
---
+++ trecia-dalis.js
@@ -0,0 +1,19 @@
+window.onload = function () {
+    function trigubaSuma (a, b) {
+    return 3 * (a + b);
+}
+    let a = 3;
+    let b = 5;
+    trigubaSuma = 3 * (a + b);
+    alert(trigubaSuma); 
+    let c = -2;
+    let d = 11;
+    trigubaSuma = 3 * (c + d);
+    console.log(trigubaSuma);
+    let sk1 = 50;
+    let sk2 = 20;
+    trigubaSuma = 3 * (sk1 + sk2);
+    alert(trigubaSuma);
+}
+
+
